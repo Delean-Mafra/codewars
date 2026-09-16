900fd0b224a7810f0a3ae3ab31a6fe2a002b7a61
Delean Mafra <170483770+Delean-Mafra@users.noreply.github.com>
2026-09-15 21:48:13 -0300

    Update Thinking & Testing : Operator hidden in a string.py

--- Thinking & Testing : Operator hidden in a string.py
+++ Thinking & Testing : Operator hidden in a string.py
@@ -1,13 +1,22 @@
 def testit(dm_s):
-    if not dm_s:
+    if not dm_s or len(dm_s) % 2 == 0:
         return None
-    dm_mapping = {'a': '1', 'b': '-', 'c': '*', 'd': '*'}
-    dm_res = []
+    
+    dm_expr = []
     for dm_i, dm_char in enumerate(dm_s):
-        if dm_i > 0 and dm_s[dm_i - 1] == 'a' and dm_char == 'a':
-            dm_res.append('+')
-        dm_res.append(dm_mapping.get(dm_char, dm_char))
-    dm_val = eval("".join(dm_res))
-    if dm_val <= 0:
-        return dm_val
-    return (dm_val + 1) // 2
+        dm_val = ord(dm_char) - 96
+        if dm_i % 2 == 0:
+            dm_expr.append(str(dm_val))
+        else:
+            dm_op = dm_val % 4
+            if dm_op == 1:
+                dm_expr.append('+')
+            elif dm_op == 2:
+                dm_expr.append('-')
+            elif dm_op == 3:
+                dm_expr.append('*')
+            else:
+                dm_expr.append('/')
+                
+    dm_res = eval("".join(dm_expr))
+    return int(dm_res) if isinstance(dm_res, float) and dm_res.is_integer() else dm_res
